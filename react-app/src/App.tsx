0aad977fc433ba040943d35e8f42211fdef52a07
loki-777 <lokeshgautham13@gmail.com>
2024-10-03 14:51:27 -0400

    Button, invoking change in parent component based on child component.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,20 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const [alertVisibe, setAlertVisibe] = useState(false);
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
+      {alertVisibe && (
+        <Alert onClose={() => setAlertVisibe(false)}>My Alert</Alert>
+      )}
+
+      <Button
+        children="Alert Button"
+        color="danger"
+        onClick={() => setAlertVisibe(true)}
       />
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
     </div>
   );
 }
